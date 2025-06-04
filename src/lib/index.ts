4df480fda630a568468042ff5209ff5c29a00827
Mat70x7 <mmoxam@hotmail.com>
2025-06-03 23:23:54 -0500

    fix: link button package fix

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 // Reexport your entry components here
 // lib/index.js
 export * from './button/index.js';
+export * from './link-button/index.js';
 export * from './text-input/index.js';
 // Add more components here...
