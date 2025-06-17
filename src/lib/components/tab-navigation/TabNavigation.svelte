b3d2cc85870fd6be3924e31f13edf19bdc4b7eff
Mat70x7 <mmoxam@hotmail.com>
2025-06-17 12:27:21 -0500

    patch: updating disabled state for button

--- src/lib/components/tab-navigation/TabNavigation.svelte
+++ src/lib/components/tab-navigation/TabNavigation.svelte
@@ -19,12 +19,12 @@
 </script>
 
 <script lang="ts">
-    let { links, preload=true, reload=false, divClasses, ...props }: TabNavigationProps = $props();
+    let { links=$bindable(), preload=true, reload=false, divClasses, ...props }: TabNavigationProps = $props();
 </script>
 
 <div class={twMerge("flex flex-row flex-wrap gap-4 justify-start xxxs:justify-center items-start py-2 px-4 mt-8 font-semibold", divClasses)}>
     {#each links as link}
-    <a class={twMerge("px-4 py-1 hover:border-primary-secondary-500 hover:border-b-2 text-gray-900 bg-transparent hover:bg-transparent rounded-none", link.active ? "border-primary-600 border-b-2": "", props.class)}
+    <a class={twMerge("px-4 py-1 hover:border-secondary-500 hover:border-b-2 text-gray-900 bg-transparent hover:bg-transparent rounded-none", link.active ? "border-primary-600 border-b-2": "", props.class)}
         data-sveltekit-reload={reload} data-sveltekit-preload-data={preload ? 'hover' : false}
         href={link.href}>
         {link.text}
