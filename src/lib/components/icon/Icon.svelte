64bf73739c1758e6636072952f66aa6eacfbe3ad
Mat70x7 <mmoxam@hotmail.com>
2025-09-26 17:37:57 -0500

    add: Adding Icon component and button support

---
+++ src/lib/components/icon/Icon.svelte
@@ -0,0 +1,27 @@
+<script lang="ts" module>
+    import type { ClassNameValue } from 'tailwind-merge';
+    /**
+     * Props for the Icon component. See instructions: https://iconify.design/docs/usage/css/tailwind/tailwind4/
+     *
+     * @prop {string} icon - The icon string to be used in the span, example: icon-[ion--add-circle]     
+     * @prop {() => void} [onclick] - Event handler for click events.
+     * @prop {Snippet} [class] - Css classes for the input element.
+     */
+    export type IconProps = {
+        icon: string;
+        onclick?: (event: MouseEvent) => void;
+        class?: ClassNameValue;
+    };
+
+</script>
+
+<script lang="ts">
+  import {twMerge} from 'tailwind-merge';
+  
+  let { icon, onclick, ...restProps }: IconProps = $props();
+
+</script>
+
+<span class={twMerge("block shrink-0", icon, restProps.class)}></span>
+
+
